0df8fdfc9474b4e018aea6f69b234d42df38b78e
pavithram1995 <48580521+pavithram1995@users.noreply.github.com>
2019-06-26 13:27:25 +0530

    Add files via upload

--- collFrontEnd/WebContent/pages/job_pages/JobController.js
+++ collFrontEnd/WebContent/pages/job_pages/JobController.js
@@ -1,10 +1,10 @@
 myApp.controller("JobController",function($scope,$location,$rootScope,$http)
 {
-	$scope.job={"jobId":0,"jobDesignation":"","companyName":"","ctc":0,"jobLocation":"","lastDate":"","skills":""};
+	$scope.job={"jobId":0,"jobDesignation":"","username":"","companyName":"","ctc":0,"jobLocation":"","lastDate":"","skills":"","status":""};
 	
 	$rootScope.jobdata;
 	
-	$scope.jobs;
+	$rootScope.jobId;
 	
 	$scope.publishJob=function()
 	{
@@ -36,6 +36,44 @@ myApp.controller("JobController",function($scope,$location,$rootScope,$http)
 			console.log('Error Occured');
 		});
 	}
+	
+	$scope.deleteJob=function(jobId)
+	{
+		console.log('Job Deleted');
+		$http.delete('http://localhost:8080/collMiddleWare/deleteJob/'+jobId)
+		.then(function(response)
+				{
+			listJobs();
+			alert('Job Deleted');
+			$location.path("/showJob");
+				},
+				function(errresponse)
+				{
+					console.log('Error Occured');
+					alert('Error Occured while Deleting Job');
+			
+				});	
+	}
+	
+	$scope.apply=function(jobId)
+	{
+		$scope.job.username=$rootScope.currentUser.username;
+		$http.get('http://localhost:8080/collMiddleWare/applyJob/'+jobId)
+		.then(function(response)
+				{
+			listJobs();
+			alert('Job Applied');
+			console.log('Job Applied');
+			$location.path("/showJob");
+				},
+				function(errresponse)
+				{
+					console.log('Error Occured');
+					alert('Error Occured while Applying Job');
+			
+				});	
+	}
+	
 	listJobs();
 	
 });
